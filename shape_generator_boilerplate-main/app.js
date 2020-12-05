5758bfbb1f9b18a6ae6b6ff2989ee7a172e6b66a
kamlesh2009 <71686631+kamlesh2009@users.noreply.github.com>
2020-12-05 20:31:14 +0800

    Add files via upload

---
+++ shape_generator_boilerplate-main/app.js
@@ -0,0 +1,25 @@
+const button=document.getElementById("button")
+var numberInShape=1;
+var oldValue=0;
+button.onclick=()=>{
+    let number= document.getElementById("number").value;
+    const circle=document.getElementById("circle");
+    const square=document.getElementById("square");
+    const rectangle=document.getElementById("rectangle");
+    const $box =document.getElementById("box");
+   number= Number(oldValue)+Number(number)
+   for(let j=numberInShape;j<=number;j++){
+       var shape=document.createElement("div")
+       shape.innerHTML+=numberInShape
+       if(square.checked)
+        shape.classList.add("square")
+       else if(circle.checked)
+        shape.classList.add("circle")
+       else if(rectangle.checked)
+        shape.classList.add("rectangle")
+       
+        $box.appendChild(shape)
+       numberInShape++;
+       oldValue=document.getElementById("box").lastElementChild.innerHTML
+   }
+}
